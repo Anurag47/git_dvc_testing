a558f76f32b48746ffda3a4a007a21139703cf06
Anurag47 <b.anurag147@gmail.com>
2025-01-01 19:50:45 +0530

    DVC Commit #1

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
-x='Test File2'
-print(x)
+import pandas as pd
+import os
 
-s=2
+d = {'col1':[1,2,3],'col2':['a','b','c']}   
+df = pd.DataFrame(data=d)
+
+data_dir = 'data'
+os.makedirs(data_dir, exist_ok=True)
+df.to_csv(os.path.join(data_dir,'test.csv'),index=False)

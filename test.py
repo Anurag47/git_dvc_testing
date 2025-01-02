5af24ff5d7c9a2b9c04242a206e872d256e3dc70
Anurag47 <b.anurag147@gmail.com>
2025-01-02 09:10:11 +0530

    3rd change to data in dvc

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import os
 
-d = {'col1':[1,2,3,4],'col2':['a','b','c','d']}   
+d = {'col1':[1,2,3,4,5],'col2':['a','b','c','d','e']}   
 df = pd.DataFrame(data=d)
 
 data_dir = 'data'
